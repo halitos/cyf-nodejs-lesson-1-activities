4e054412d3dda3a60f030c1c5369a825bf23c60f
Halit <60817499+halitos@users.noreply.github.com>
2020-08-09 10:54:42 +0100

    exe 5 done

--- server.js
+++ server.js
@@ -2,15 +2,48 @@ const express = require("express");
 const app = express();
 const port = 9090;
 
-app.get("/", (request, response) => {
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/cyf", (request, response) => {
   response.json({
     recipient: "CodeYourFuture",
     message: "Hello there!",
   });
 });
 
-app.get("/hello-cyf", (req, res) => {
-  res.send("Hello Code Your Future!");
+const songs = [
+  {
+    id: 1,
+    title: "Under Pressure",
+    singer: "Queen & David",
+  },
+  {
+    id: 2,
+    title: "Wild World",
+    singer: "Cat Stevens",
+  },
+  {
+    id: 3,
+    title: "Nothing Else Matters",
+    singer: "Metallica",
+  },
+];
+
+app.get("/songs", (request, response) => {
+  response.json({
+    songs,
+  });
+});
+
+app.get("/songs/:id", (request, response) => {
+  let id = Number(request.params.id);
+  let mySong = songs.find((song) => song.id === id);
+  response.json({
+    msg: `The client made a request for song with the id: ${id}`,
+    mySong,
+  });
 });
 
 app.listen(port, () => {
